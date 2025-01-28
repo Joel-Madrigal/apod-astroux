a6a25b38592b90f259b7b24a469b0283d3f2238f
Vivek Bhookya <v.bhookya98@gmail.com>
2025-01-27 23:32:39 -0600

    template js fetch

--- apod-fetch.js
+++ apod-fetch.js
@@ -1,1 +1,26 @@
 alert('hi')
+
+let key = '';
+let api = '' + key
+
+
+// http://youmightnotneedjquery.com/#json
+let request = new XMLHttpRequest();
+request.open("GET", api, false);
+
+request.onload = function () {
+  if (request.status >= 200 && request.status < 400) {
+    // Success!
+    let data = JSON.parse(request.responseText);
+    console.log(data)
+  } else {
+    // We received an error from the api!
+  }
+};
+
+request.onerror = function () {
+  // There was another error, not from the api,
+  alert('Error');
+};
+
+request.send();
